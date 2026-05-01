874f12cbcc6cb6a6f2a3bce0a9fdd31c0b57b690
unknown <Pavitragv24cs@srishakthi.ac.in>
2026-05-01 13:43:35 +0530

    remove wrangler config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  vite: {
-    build: {
-      outDir: "dist",
-    },
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: "dist",
   },
 });
