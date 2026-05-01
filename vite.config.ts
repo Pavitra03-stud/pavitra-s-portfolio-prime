06d54c7f84465d1be8393496d54aa05d5b848614
unknown <Pavitragv24cs@srishakthi.ac.in>
2026-05-01 19:09:41 +0530

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    outDir: "dist",
-  },
-});
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
+export default defineConfig();
